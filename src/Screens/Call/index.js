6d21035f8e172ab274f41e2cf9cf48ed5fce57ee
Thomas PHIMANESONE <thomas.phimanesone@kleegroup.com>
2020-07-06 16:02:31 +0200

    all hard code done

--- src/Screens/Call/index.js
+++ src/Screens/Call/index.js
@@ -1,14 +1,23 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text,Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 function CallScreen({route,navigation}){
-    return(
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Test Call</Text>
-      </View>
-    );
+  let phoneNumber = '0651507634'
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Êtes vous sûr de vouloir appeler Yassine ?</Text>
+      <Button
+        title="Oui"
+        onPress={() => Linking.openURL(`tel:${phoneNumber}`)}>
+      </Button>
+      <Button
+        title="Non"
+        onPress={() => navigation.navigate("Accueil")}>
+      </Button>
+    </View>
+  );
 }
 
 export default CallScreen;
